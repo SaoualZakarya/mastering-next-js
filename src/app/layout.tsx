b89788718e50a2f668ca6c3dd9bbd62322d89e8a
zakaryasaoual@gmail.com <zakaryasaoual@gmail.com>
2024-07-03 14:42:39 +0100

    initialization

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,7 @@
 import './globals.css'
-import type { Metadata } from "next"
 import {Inter} from 'next/font/google'
 
 const inter = Inter({subsets:['latin']});
-
-export const metadata:Metadata = {
-  title:'Home',
-  description:'Home page'
-}
 
 export default function RootLayout({
   children,
--- src/app/lib/getUserPosts.ts
+++
@@ -1,9 +0,0 @@
-
-export default async function getUserPosts (userId:string)  {
-
-  const res = await fetch(`https://jsonplaceholder.typicode.com/posts?userId=${userId}`)
-
-  if(!res.ok) return undefined
-
-  return res.json() ;
-}
