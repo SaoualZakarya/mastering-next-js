005bcb11d1c9d338876af07511d416ad67765c43
zakaryasaoual@gmail.com <zakaryasaoual@gmail.com>
2024-07-04 09:55:35 +0100

    Add the loading , error and the complete project

--- src/app/[searchTerm]/page.tsx
+++ src/app/[searchTerm]/page.tsx
@@ -30,17 +30,19 @@ export default async function searchResults  ({params:{searchTerm}}:Props)  {
 
   const data = await wikiData ;
 
+  const displayTerm = searchTerm.replaceAll('%20', ' ')
+
   const results : Result[] | undefined = data?.query?.pages
   
   const content = (
     <main className="bg-slate-200 text-black mx-auto max-w-lg py-1 min-h-screen">
-            {results
-                ? Object.values(results).map((result,i) => {
-                    return <Item key={i} result={result} />
-                })
-                : <h2 className="p-2 text-xl">{`${searchTerm} Not Found`}</h2>
-            }
-        </main>
+        {results
+            ? Object.values(results).map((result,i) => {
+                return <Item key={i} result={result} />
+            })
+            : <h2 className="p-2 text-xl text-center">  <span className="font-bold"> {"'"+ displayTerm + "'"} </span>  Not found</h2>
+        }
+    </main>
   )
 
   return content ;
